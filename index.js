a1cd5a2778a4c0fba7d9bfdf6f2c20556a202759
popeyewrener <popwren@gmail.com>
2024-04-24 03:50:01 +0530

    api done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 const fs = require('fs');
 const env = require('dotenv').config();  
+const express = require('express');
+const http = require('http');
 
 // Define the path to the .txt file
 const contextPath = './context.txt';
@@ -12,9 +14,12 @@ const primingPath = './priming.txt';
 const MODEL_NAME = "gemini-pro";
 const API_KEY = process.env.GOOGLE_API_KEY;
   
-async function run() {
-    context = "";
-    priming = "";
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const httpServer = http.createServer(app);
+context = "";
+priming = "";
     fs.readFile(primingPath, 'utf8', (err, data) => {
         if (err) {
             // Handle the error if the file cannot be read
@@ -33,6 +38,9 @@ async function run() {
             context = data;
         }
     });
+
+async function run() {
+    
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
@@ -63,6 +71,7 @@ async function run() {
     ];
     const prompt = "First-Order Predicate Logic: Every apple is red";
     const final_prompt = priming + context + prompt;
+    //console.log(final_prompt);
 
     
 
@@ -81,4 +90,55 @@ async function run() {
     console.log(response.text());
 }
 
-run();
+const apicall = async (data) => {
+    const {prompt}= data;
+    let p = prompt;
+    const genAI = new GoogleGenerativeAI(API_KEY);
+    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+    const generationConfig = {
+        temperature: 0.9,
+        topK: 1,
+        topP: 1,
+        maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+       
+    ];
+     p = "First-Order Predicate Logic: "+p.toString();
+     p2 = p.toString();
+     
+    const final_prompt = priming + context + p2;
+    //console.log(final_prompt);
+
+    
+
+    const parts = [
+        {text: final_prompt},
+
+    ];
+
+    const result = await model.generateContent({
+        contents: [{ role: "user", parts }],
+        generationConfig,
+        safetySettings,
+    });
+    console.log(result.response); 
+    if (result.response.candidates[0].finishReason === "HARM_BLOCKED" || result.response.candidates[0].finishReason === "SAFETY"|| result.response.candidates[0].finishReason === "STOPPING_CRITERIA") {
+        return "Sorry, I can't generate content based on the prompt. Please try another prompt."
+    }
+
+    return result.response.text();
+
+};
+//run();
+app.post('/generate', async (req, res) => {
+    const data = req.body;
+    const response = await apicall(data);
+  res.json({ response });
+});
+
+httpServer.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port 3000');
+});
